49f9b4201af66b357b5c4b0cdd4ae776ee18eba8
Felipe Sangiorge <felipe.sangiorge@gmail.com>
2017-07-11 17:13:26 -0300

    Primeiro ws

---
+++ backend/loader.js
@@ -0,0 +1,21 @@
+const port = 3003
+
+const bodyParser = require('body-parser')
+const express = require ('express')
+const server = express()
+
+server.use(bodyParser.urlencoded({extended:true}))
+server.use(bodyParser.json())
+
+server.listen(port,function () {
+  console.log(`Backend running on port: ${port}.`)
+})
+
+server.use(function(req,res,next) {
+  console.log("meu md1");
+  next()
+})
+
+server.use(function(req,res,next) {
+  res.send("funcionou md 2")
+})

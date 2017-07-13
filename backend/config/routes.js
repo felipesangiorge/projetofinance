aefb255e5c5a07e7140d304ed3c6e96824d4561c
Felipe Sangiorge <felipe.sangiorge@gmail.com>
2017-07-13 11:49:26 -0300

    Angular, Node , express

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -5,4 +5,9 @@ module.exports = function(server){
     //API ROUTERS
     const router = express.Router()
     server.use('/api',router)
-}
+    //Rotas da api
+
+    const billingCycleService= require('../api/billingCycle/billingCycleService')
+    billingCycleService.register(router,'/billingCycles')
+
+    }

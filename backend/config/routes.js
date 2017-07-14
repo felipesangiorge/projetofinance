04c6b3c4d8e9c1dd65b090414033eeb45f38361d
Felipe Sangiorge <felipe.sangiorge@gmail.com>
2017-07-14 15:50:22 -0300

    web service

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -5,9 +5,11 @@ module.exports = function(server){
     //API ROUTERS
     const router = express.Router()
     server.use('/api',router)
-    //Rotas da api
 
+    //Rotas da api
     const billingCycleService= require('../api/billingCycle/billingCycleService')
     billingCycleService.register(router,'/billingCycles')
 
+    const billingSummaryService = require('../api/billingSummary/billingSummary')
+    router.route('/billingSummary').get(billingSummaryService.getSummary)
     }

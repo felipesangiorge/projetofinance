42dcbccf09afcb9a1c05314789c436a4eb2ec52f
Felipe Sangiorge <felipe.sangiorge@gmail.com>
2017-09-13 17:54:53 -0300

    Paginação

--- angular1/app/common/components/paginator.js
+++ angular1/app/common/components/paginator.js
@@ -6,26 +6,31 @@
     },
     controller:[
       '$location',
-      function ($location) {
+      'gridSystem',
+      function ($location,gridSystem) {
+
         this.$onInit = function(){
+
           const pages = parseInt(this.pages) || 1
-          this.pagesArray = Array(pages).fill(0).map((e,i) => i+1)
+          this.pagesArray = Array(pages).fill(0).map((e,i) => i + 1)
+          this.needPagination = this.pages > 1
+          this.hasNext= this.current < this.pages
         }
 
-        this.current= parseInt($location.search().page) || 1
-        this.needPagination = this.pages > 1
-        this.hasPrev= this.current > 1
-        this.hasNext= this.current < this.pages
 
+        this.current= parseInt($location.search().page) || 1
+        this.hasPrev= this.current > 1
         this.isCurrent= function(i){
           return this.current == i
         }
+
+
       }
     ],
     template:`
     <ul ng-if="$ctrl.needPagination" class="pagination">
       <li ng-if="$ctrl.hasPrev">
-        <a href="{{ $ctrl.url }}?page={{ $ctrl.current -1 }}">Anterior</a>
+        <a href="{{ $ctrl.url }}?page={{ $ctrl.current - 1}}">Anterior</a>
       </li>
 
       <li ng-class="{active: $ctrl.isCurrent(index)}" ng-repeat="index in $ctrl.pagesArray">
@@ -33,7 +38,7 @@
       </li>
 
       <li ng-if="$ctrl.hasNext">
-        <a href="{{$ctrl.url}}?page={{ctrl.current + 1}}">Próximo</a>
+      <a href="{{ $ctrl.url }}?page={{ $ctrl.current + 1}}">Próximo</a>
       </li>
     </ul>
     `
